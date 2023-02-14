d8150f4cfff226d6db793d027a47b35dcffaa2c3
AllisonJoanine <122819284+AllisonJoanine@users.noreply.github.com>
2023-02-13 23:29:18 -0300

    Add files via upload

--- SITE/HOME/opcoes/cadastrosgr/function.js
+++ SITE/HOME/opcoes/cadastrosgr/function.js
@@ -16,3 +16,28 @@ select.addEventListener("change", function() {
     }
   }
 });
+
+/////////////
+
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+//espaço
+
+const toggleSubmenuLinks = document.querySelectorAll('.toggle-submenu');
+
+toggleSubmenuLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const submenu = link.nextElementSibling;
+    submenu.classList.toggle('show-submenu');
+  });
+});
+
+//sub
+
+
--- SITE/TELA DE LOGIN/js/function.js
+++ SITE/TELA DE LOGIN/js/function.js
@@ -1,11 +1,12 @@
-if (isset($_POST['username']) && isset($_POST['password'])) {
+/*if (isset($_POST['username']) && isset($_POST['password'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  // Valide as credenciais do usuário aqui
-
-  // Redirecione o usuário para a página de destino, por exemplo:
-  header('location:../HOME/home.html');
+  header('location: file:///C:/Users/allison_adm/Desktop/SITE/HOME/home.html');
   exit;
 }
+*/
 
+document.getElementById("button").addEventListener("click", function() {
+  window.location.href = "../SITE/HOME/home.html";
+});
